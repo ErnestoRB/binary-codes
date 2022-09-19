139a0b1c6a1bfe22bcf6758e8ae3eb168606d9c1
Haitore <ender12as@gmail.com>
2022-09-18 20:05:51 -0500

    5. Manchester

--- mnchstr.go
+++ mnchstr.go
@@ -1,38 +1,54 @@
 package main
 
-func mandiff(b uint8) {
+var up int
+
+func mnchstr(b uint8) {
+	var prevVal uint8 = 0
 	var transition= false
 	arr := toBinaryArray(b)
 	for i := 0; i < 8; i++ {
 		nBit := arr[i]
-		drawMandiffBit(nBit, &transition, i)
+		drawMnchstrBit(nBit, prevVal, &transition, i)
+		prevVal = nBit
 	}
 }
 
-func drawMandiffBit(b uint8, transition *bool, pos int) {
+func drawMnchstrBit(b, prevVal uint8, transition *bool, pos int)  {
+	if pos == 0 {
+		if b == 1 {
+			up = 0;
+		} else{
+			up = 1;
+		}
+	}
+
+	if b == 1 {
+		if up == 1 {
+			drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0.5,0})
+			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0,1})
+			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0.5,0})
+		} else {
+			drawLine(Dc, Vector{ X: float64(pos), Y: 1}, Vector{0.5,0})
+			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0,-1})
+			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0.5,0})
+		}
+	}
+
 	if b == 0 {
-		if !(*transition) {
-			drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0,1})
+		if up == 0 {
+			if prevVal == 0 {
+				drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0,1})
+			}
 			drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0.5,0})
 			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0,1})
 			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0.5,0})
 		} else {
-			drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0,1})
+			if prevVal == 0 {
+				drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0,1})
+			}
 			drawLine(Dc, Vector{ X: float64(pos), Y: 1}, Vector{0.5,0})
 			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0,-1})
 			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0.5,0})
 		}
-	} else if b == 1 {
-		*transition = !(*transition)
-		if !(*transition){
-			drawLine(Dc, Vector{ X: float64(pos), Y: 0}, Vector{0.5,0})
-			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0,1})
-			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0.5,0})
-		} else {
-			drawLine(Dc, Vector{ X: float64(pos), Y: 1}, Vector{0.5,0})
-			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 1}, Vector{0,-1})
-			drawLine(Dc, Vector{ X: (float64(pos) + 0.5), Y: 0}, Vector{0.5,0})
-		}
-		
 	}
 }
